80f70773e858df0b75c0cbc040b28c0057eaef07
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2023-04-17 00:20:16 +0200

    type orm conn + docker composer simplify

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [TypeOrmModule.forRoot({
+		type: 'postgres',
+		host: 'database',
+		port: 5432,
+		username: 'root',
+		password: '$42transcendence$',
+		database: 'transcendence',
+		entities: [__dirname + '/**/*.entity{.ts,.js}'],
+		synchronize: true
+	})],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
