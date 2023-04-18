ddccb8aca8f1384fe3b9ea888edcb79d8555bd4c
aliciamurma <aliciamurciamaya@gmail.com>
2023-04-18 19:10:11 +0200

    auth inprogress

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -7,21 +7,21 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(':code')
+  findOne(@Param('code') id: string) {
     return this.userService.findOne(+id);
   }
 
+
+
+
+
+  
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
