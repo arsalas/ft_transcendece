69bc8bc8e00aad87eb73e12c2e022038d5247ad0
Alberto Ramirez <a.ramir.sa@gmail.com>
2023-04-19 18:16:58 +0200

    api login

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,38 +1,12 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Res, Next } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
 	constructor(private readonly userService: UserService) { }
 
-	@Get()
-	findAll() {
-		return "user";
-		// res.redirect(301, 'http://localhost/test');
-
-		// return;
-		// return this.userService.findAll();
+	@Get("signIn/:code")
+	async signIn(@Param('code') code: string) {
+		return await this.userService.signIn(code);
 	}
-
-	//   @Get(':code')
-	//   findOne(@Param('code') id: string) {
-	//     // return this.userService.findOne(+id)S;
-	//   }
-
-
-
-
-
-
-	//   @Patch(':id')
-	//   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-	//     // return this.userService.update(+id, updateUserDto);
-	//   }
-
-	//   @Delete(':id')
-	//   remove(@Param('id') id: string) {
-	//     // return this.userService.remove(+id);
-	//   }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import axios from 'axios';
 
 @Injectable()
 export class UserService {
-  
+
+
+  async signIn(code: string) {
+    try {
+
+      // 1 - Pedir ala api de 42 que identifique al usuario
+      const body = {
+        client_id: "u-s4t2ud-002f8307ed61fa03609f72d495d3a6e7efe6c446b08744c9b33e4ea27e613829",
+        grant_type: "authorization_code",
+        client_secret: "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",
+        code, // su valor va a ser el mismo que la variable
+        redirect_uri: "http://localhost"
+      }
+      console.log(body)
+      const response:Axios<IOauth> = await axios.post("https://api.intra.42.fr/oauth/token", body);
+      console.log(response)
+    } catch (error) {
+      console.log("ERROR");
+      // console.log({error});
+    }
+
+    // 2 - Buscar en nuestra BBDD si existe el usuario
+    // 3a - Si existe devolver el usuario
+    // 3b - Crear un nuevo usuario
+
+  }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  
+
 }
