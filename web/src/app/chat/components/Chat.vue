d67324644976569b60b0d6867ae883196fd98d1a
aliciamurma <aliciamurciamaya@gmail.com>
2023-04-25 19:31:38 +0200

    chat

--- web/src/app/chat/components/Chat.vue
+++ web/src/app/chat/components/Chat.vue
@@ -1,20 +1,7 @@
-    <!-- // variable con los chats
-    /*
-    interfaz
-     {
-        login: string,
-        message:string,
-        date: string casts new Date() tiempo relativo,
-    
-     }
-     */ -->
-    
-    <!-- </script>
-    
-    
-    <style lang="scss" scoped>
-    </style> -->
-
+<!-- To cast a string to Date
+const str = '2024-07-21';
+const date = new Date(str);
+date.toDateString(); -->
 
 <template>
     <div class="chat-container">
@@ -23,21 +10,53 @@
                 <span>Today, 9:00 PM</span>
             </div>
 
-            <div class="bubble other">
-                <div class="username">Alberto</div>
-                Hola Alicia
+            <div class="bubble" :class="chat.login == 'amurcia-' ? 'me' : 'other'" v-for="chat in chats">
+                <div v-if="chat.login != 'amurcia-'" class="username">{{ chat.login }}</div>
+                {{ chat.message }}
+
             </div>
 
-            <div class="bubble me">
-                Hola Alberto
-            </div>
+
+            <button @click="addMsg">ADD</button>
+
         </div>
     </div>
 </template>
+
 <script lang='ts' setup>
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+
+interface IChat {
+    login: string;
+    message: string;
+    date: string;
+}
+
+const addMsg = () => {
+    chats.value.push({
+        login: "amurcia-",
+        message: "Hola Alberto",
+        date: "10.05.2023",
+    })
+}
+
+const chats = ref<IChat[]>([// const y no let para que no pierda la reactividad
+    {
+        login: "amurcia-",
+        message: "Hola Alberto",
+        date: "10.05.2023",
+    },
+    {
+        login: "aramirez",
+        message: "Hola Alicia",
+        date: "11.05.2023",
+    }
+]);
 
 
 </script>
+
 <style lang='scss' scoped>
 .chat-container {
     display: flex;
@@ -88,7 +107,7 @@
             border-top-left-radius: 0rem;
             box-shadow: -1px 2px 0px rgb(0, 0, 0);
 
-          
+
 
             & .username {
                 font-size: 0.8rem;
@@ -111,4 +130,7 @@
             animation-name: slideFromRight;
         }
     }
-}</style>
+
+
+}
+</style>
