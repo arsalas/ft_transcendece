42963f6dbc8332910edcc734a2567bf52f166a0b
aliciamurma <aliciamurciamaya@gmail.com>
2023-05-05 20:26:54 +0200

    chat

--- web/src/app/dashboard/pages/ChatsPage.vue
+++ web/src/app/dashboard/pages/ChatsPage.vue
@@ -1,28 +1,37 @@
 <template>
-  <div
-    style="
-      height: 100vh;
-      width: 100vw;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    ">
+  <div class="container">
     <div class="chat">
       <Chat />
     </div>
   </div>
 </template>
+
 <script lang="ts" setup>
 import { defineAsyncComponent } from 'vue';
 
 const Chat = defineAsyncComponent(
   () => import('../../chat/components/Chat.vue'),
 );
+
+//TODO
+/*
+-Cambiar esquinas
+-Color
+-Botones
+*/
 </script>
+
 <style lang="scss" scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .chat {
   padding: 20px;
-  background-color: red;
+  background-color: rgb(0, 2, 68);
 
   height: 50vh;
   width: 40vw;
