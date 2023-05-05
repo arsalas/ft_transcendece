52b96baf6614cbd44929ae97cf5e9510a9cf8ff5
aliciamurma <aliciamurciamaya@gmail.com>
2023-05-05 16:00:36 +0200

    history

--- web/src/app/dashboard/pages/profile/HistoryPage.vue
+++ web/src/app/dashboard/pages/profile/HistoryPage.vue
@@ -8,8 +8,19 @@
           :src="profile?.profile.avatar || profile?.profile.avatar42"
           alt="" />
         <div class="text data">
-          <div class="text is-large">aramirez</div>
-          <div class="text win-lost">Win</div>
+          <div class="text is-large">{{ profile?.history[0].player }}</div>
+          <div
+            :class="{
+              'win-lost': true,
+              'text-green': i == 1,
+              'text-red': i != 1,
+            }">
+            {{ i === 1 ? 'Winner' : 'Loser' }}
+            <div class="text date">
+              {{ profile?.history[0].date }}
+            </div>
+          </div>
+          <!-- <div :class="text win-lost">Winner</div> -->
         </div>
       </li>
     </ul>
@@ -40,13 +51,12 @@ const { profile } = storeToRefs(profileStore);
   align-items: center;
   flex-direction: column;
   justify-content: space-between;
-  border: solid red 1px;
 }
 
 .avatar {
   width: 9rem;
   border-radius: 100%;
-  border: var(--border);
+  border: var(--border-big);
   margin-right: 3rem;
 }
 
@@ -59,7 +69,25 @@ const { profile } = storeToRefs(profileStore);
   font-weight: 700;
 }
 
-.text-data {
+.text-green {
+  color: green;
+  text-shadow: -0.05rem -0.05rem 0 rgb(9, 99, 9),
+    0.05rem -0.05rem 0 rgb(9, 99, 9), -0.05rem 0.05rem 0 rgb(9, 99, 9),
+    0.05rem 0.05rem 0 rgb(9, 99, 9);
+}
+
+.text-red {
+  color: red;
+  text-shadow: -0.05rem -0.05rem 0 rgb(110, 5, 5),
+    0.05rem -0.05rem 0 rgb(110, 5, 5), -0.05rem 0.05rem 0 rgb(110, 5, 5),
+    0.05rem 0.05rem 0 rgb(110, 5, 5);
+}
+
+.data {
   display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  justify-content: space-around;
 }
 </style>
