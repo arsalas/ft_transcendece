99a4a30edeaab99181b48e02a6309ba6f55f0439
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2023-06-28 00:56:19 +0200

    chat layout done

---
+++ web/src/app/dashboard/pages/ChatPage.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="chat pattern">
+    <Chat :type="EChatType.Protected" :messages="messages" name="Lorem ipsum" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent } from 'vue';
+import { EChatType, IChat } from '../../../interfaces';
+
+const Chat = defineAsyncComponent(
+  () => import('../../common/components/Chat.vue'),
+);
+
+const user = {
+  login: 'amurcia-',
+  username: 'amurcia-',
+  avatar: null,
+  avatar42: 'https://cdn.intra.42.fr/users/f9d736eaefdcc160d0f742b44a8f5d4a/amurcia-.jpg',
+  status: 'offline',
+};
+
+const messages: IChat[] = [
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'aramirez',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'aramirez',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'aramirez',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'aramirez',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'amurcia-',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+  {
+    isRead: true,
+    createdAt: new Date(),
+    userId: 'aramirez',
+    message:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptate explicabo ullam ipsam pariatur tenetur eveniet cum provident ipsum unde.',
+  },
+];
+</script>
+
+<style lang="scss" scoped>
+.chat {
+  height: 100%;
+}
+</style>
